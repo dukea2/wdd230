c4e9767911937182be2ffd9d0d46bba44cae6bcb
dukea2 <belduke159@gmail.com>
2023-03-18 02:11:22 -0600

    Directory page

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -19,3 +19,70 @@ onload = function(){
 const mainnav = document.querySelector('.navigation')
 const hambutton = document.querySelector('#menu');
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// Directory Page //
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector(".cards");
+
+
+if (gridbutton) {
+    gridbutton.addEventListener("click", () => {
+        display.classList.add("grid");
+        display.classList.remove("list");
+    });
+    
+    listbutton.addEventListener("click", showList);
+    
+    function showList() {
+        display.classList.add("list");
+        display.classList.remove("grid");
+    }
+}
+
+const url = "json/data.json";
+
+// Fetch from JSON
+async function getBusinessData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayBusinesses(data.businessDirectory);
+}
+
+getBusinessData()
+
+// Display
+const displayBusinesses = (businessDirectory) => {
+    const cards = document.querySelector('article.cards');
+
+    businessDirectory.forEach((business) => {
+        let card = document.createElement('section');
+        let name = document.createElement('p');
+        let address = document.createElement('p');
+        let phone = document.createElement('p');
+        let website = document.createElement('a')
+        let image = document.createElement('img');
+
+        name.textContent = `${business.name}`;
+        name.setAttribute('id', 'name-hidden');
+
+        address.textContent = `${business.address}`;
+        phone.textContent = `${business.phone}`;
+
+        website.setAttribute('href', `${business.website}`);
+        website.textContent = `${business.webtext}`;
+        
+        image.setAttribute('src', business.imageurl);
+        image.setAttribute('alt', `${business.name} Logo`);
+        image.setAttribute('loading', 'lazy');
+        image.setAttribute('width', '200');
+
+        card.appendChild(image);
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+
+        cards.appendChild(card);
+    })
+}
