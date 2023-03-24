3a806c0def31c7cac18989d16d7fc23e8aa16ea4
dukea2 <belduke159@gmail.com>
2023-03-23 23:14:01 -0600

    weather api for chamber site

---
+++ chamber/scripts/weatherapi.js
@@ -0,0 +1,34 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('#weather-condition');
+const windSpeed = document.querySelector('#weather-wind');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Rexburg,us&units=imperial&appid=249ff5f12842112d7ea5bfdbac4565f6';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+  function displayResults(weatherData) {
+    currentTemp.innerHTML = `${weatherData.main.temp.toFixed(0)}`;
+    
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+    
+    windSpeed.innerHTML = `${weatherData.wind.speed.toFixed(1)}`;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+  }
--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,5 +1,5 @@
-const t = document.getElementById("temp").innerText;
-const s = document.getElementById("windspeed").innerText;
+const t = document.getElementById("current-temp").innerText;
+const s = document.getElementById("weather-wind").innerText;
 
 if (t <= 50 && s > 3) {
     f = Math.round(35.74 + (0.6215 * t) - (35.75 * (s**0.16)) + (0.4275 * t * (s**0.16))) + "°F";
